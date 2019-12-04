f572b6570ebbc18ada8dba3634330b6cddef1567
rene_mbp <harryloveslearning@googlemail.com>
2019-12-04 11:51:57 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,16 @@ const IndexPage = () => (
   <Layout>
     <SEO title="@relang" />
     <h1>Hello - have a good time</h1>
+    <p>
+      I'm currently working on <a href="http://ecad.fun">ecad.fun</a>.<br></br>
+      Just take a look here:
+      <Link to="/ecad-fun">Look here</Link>
+    </p>
     {/* <Link to="http://ecad.fun">ecad.fun</Link> */}
     {/* <h2>old blogs:</h2> */}
+    <Link to="/simpleNodeServer">Node Web Server</Link>|
     <Link to="/esp8266">ESP8266</Link> |<Link to="/docker">Docker</Link>|
-    <Link to="/about">About me</Link>|<Link to="/page-2/">Go to page 2</Link>
+    <Link to="/about">About me</Link>|
   </Layout>
 )
 
